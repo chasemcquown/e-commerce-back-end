9d75c480ca4e0388e3180323568028992842db6d
chasem <cpmcquown@gmail.com>
2021-08-04 19:08:48 -0700

    adds routes

--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,17 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      // string
+      type: DataTypes.STRING,
+      allowNull: true
+    }
   },
   {
     sequelize,
